58a07166635f6329e2fbd151820d908288a8b9f7
Jacob Graham <jakegraham54@gmail.com>
2022-03-11 15:25:49 -0500

    Created a base html template when loading the root content of website. Created child html templates to override the base template and stick our own html inside of it. Created empty javascript file to use eventually for web pages

--- website/views.py
+++ website/views.py
@@ -1,8 +1,8 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__) # A blueprint is a template for generating a section of a web application. Each time you apply the blueprint to a different place in your
 # application, a new version of the blueprint's structure will be created.
 
 @views.route('/') # Defining the route for the index endpoint
 def home():
-    return "<h1>Test</h1>"
+    return render_template("home.html")
