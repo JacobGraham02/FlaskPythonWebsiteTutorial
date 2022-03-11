4221da3214c0abff989726628e8b92b39e386cbb
Jacob Graham <jakegraham54@gmail.com>
2022-03-11 15:56:47 -0500

    Modified all html template pages to inherit from parent template. Test passing variables from auth.py to login.html template page

--- website/auth.py
+++ website/auth.py
@@ -1,16 +1,19 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__) # A blueprint is a template for generating a section of a web application. Each time you apply the blueprint to a different place in your
 # application, a new version of the blueprint's structure will be created.
 
+
+
 @auth.route('/login') 
 def login():
-    return "<p>Login</p>"
+    # Second argument to render_template passes variables to the template defined in the first argument. 
+    return render_template("login.html", text="Testing", user="Jacob")
 
 @auth.route('/logout')
 def logout():
-    return "<p>Logout</p>"
+    return render_template("logout.html")
 
 @auth.route('/sign-up')
 def sign_up():
-    return "<p>Sign up</p>"
+    return render_template("signup.html")
