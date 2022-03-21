9d6dce093e84824fd937092467a9928b7c6fc48c
Jacob Graham <jakegraham54@gmail.com>
2022-03-21 15:48:12 -0400

    Fixed typos in base.html and login.html crashing the program due to syntax errors. Finished creating the database initialization in __init__.py, and created the database models in models.py

--- website/__init__.py
+++ website/__init__.py
@@ -1,9 +1,15 @@
 # Start the youtube video from 9:10: https://www.youtube.com/watch?v=dam0GPOAvVI&list=LL&index=3
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__) # represents name of file that was ran when Flask is initialized
     app.config['SECRET_KEY'] = '(nY*AtNuz$KBm6B/FT[w,[79K9h<x3r_}bfgcUz{%/]uS^ne' # Secure session and cookie information for our website. Specifically, this is going to be our secret key. 
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}' 
+    db.init_app(app)
     
     from .views import views
     from .auth import auth
---
+++ website/models.py
@@ -0,0 +1,24 @@
+# Importing an object {db} from the current package (current folder)
+from . import db
+# UserMixin is a module that can be used by our user objects to give properties specific to flask logins. Is inherited by user objects
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.String(10000))
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    
+    # Foreign key
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    first_name = db.Column(db.String(150))
+    
+    # Completing the foreign key relationship with the Note class.
+    notes = db.relationship('Note')
+
+
