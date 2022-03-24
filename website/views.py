4f43e90adbca116fb7c3e7981c88447c55e45d6c
Jacob Graham <jakegraham54@gmail.com>
2022-03-24 16:40:40 -0400

    Added functionality to delete a user note

--- website/views.py
+++ website/views.py
@@ -1,7 +1,8 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, jsonify
 from flask_login import login_required, current_user
 from .models import Note
 from . import db
+import json
 
 views = Blueprint('views', __name__) # A blueprint is a template for generating a section of a web application. Each time you apply the blueprint to a different place in your
 # application, a new version of the blueprint's structure will be created.
@@ -20,3 +21,14 @@ def home():
             db.session.commit()
             flash("Your note was added", category='success')
     return render_template("home.html", user=current_user) # Reference the currently logged in user in the home.html template. 
+
+@views.route('/delete-note', methods=['POST']) 
+def delete_note():
+    note = json.loads(request.data)
+    noteId = note['noteId']
+    note = Note.query.get(noteId)
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+    return jsonify({})
